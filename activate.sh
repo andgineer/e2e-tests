507160dd5eee353a80d31207f7d49205f837eda9
Andrei Sorokin <Andrei_Sorokin2@epam.com>
2024-05-03 06:41:12 +0200

    https://github.com/andgineer/e2e-tests/issues/6 cleanup

--- activate.sh
+++ activate.sh
@@ -3,7 +3,7 @@
 # "Set-ups or/and activates development environment"
 #
 
-VENV_FOLDER="venv"
+VENV_FOLDER=".venv"
 PRIMARY_PYTHON_VERSION="3.12"  # sync with .github/workflows/docs.yml&static.yml
 
 RED='\033[1;31m'
